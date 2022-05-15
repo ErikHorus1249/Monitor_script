ea0d710b20b8edd1d837e185f916505ecde14ed6
anhnt376 <anhnt376@ftp.com.vn>
2022-05-15 11:51:31 +0700

    update

--- NginX/Linux/nginxreload.sh
+++ NginX/Linux/nginxreload.sh
@@ -1,51 +1,42 @@
 #!/bin/bash
 
 #--------------------------DEFAULT VARIABLE------------------#
-LOGFILE="overheat.log"
-SPLUNK_HOME="/opt/splunk/bin/splunk"
+LOGFILE="nginxrl.log"
+LOGDIR="/var/log/nginxrl"
+LOGPATH="${LOGDIR}/${LOGFILE}"
+# SPLUNK_HOME="/opt/splunk/bin/splunk"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 #-------------------------------COLOR------------------------#
 YL='\033[1;33m'
 PL='\033[0;35m'
 NC='\033[0m' # No Color
-#-------------------------------HELP-------------------------#
-Help()
-{
-   # Display Help
-   echo -e "Syntax: ${YL}overheat${NC} [-s|l|h]"
-   echo "options:"
-   echo -e "-s     Splunk home directory, default: ${PL}/opt/splunk/bin/splunk"${NC}
-   echo -e "-l     Log directory, default: ${PL}$PWD/overheat.log${NC}"
-   echo "-h     Help!"
-   echo
-}
-#------------------------------OPTIONS----------------------# 
-while getopts s:l:h option
-do
-    case "${option}" in
-        l) LOGFILE=${OPTARG};;
-        s) SPLUNK_HOME=${OPTARG};;
-        h) # display Help
-         Help
-         exit;;
-    esac
-done
-#-----------------------------MAIN LOOP----------------------#
+
+#
+if [ -d $LOGDIR ] 
+then
+    echo "The log file was stored at ${LOGPATH}" 
+else
+    echo "The log file is stored at ${LOGPATH}" 
+    sudo mkdir $LOGDIR > /dev/null
+    sudo touch $LOGPATH > /dev/null
+    sudo chown $USER:$USER $LOGPATH > /dev/null 
+fi
+#-------------------------------MAIN PROCESS------------------------#
 while true; 
 do  
+    USED_CPU=`grep 'cpu ' /proc/stat | awk '{print ($2+$4)*100/($2+$4+$5)}'`;
     USED_RAM=`free | awk 'NR == 2 {print ($2 -$7)/$2 * 100.0}'`; 
-    if [ ${USED_RAM%.*} -ge 80 ]
+    if [ ${USED_RAM%.*} -ge 80 ] || [ ${USED_CPU%.*} -ge 80 ]
     then 
-        # service splunk status | grep 'active (running)' > /dev/null 2>&1
-        echo "$(date "+%Y-%m-%d %H:%M:%S") Caution! High RAM usage, in use is: ${USED_RAM%.*}%" >> $LOGFILE
-        # restart splunk 
-        sudo $SPLUNK_HOME restart > /dev/null
-        echo "$(date "+%Y-%m-%d %H:%M:%S") Retarted Splunk!" >> $LOGFILE
-        echo "$(date "+%Y-%m-%d %H:%M:%S") Sleep for 2 minutes" >> $LOGFILE
+        echo "$(date "+%Y-%m-%d %H:%M:%S") Caution! High resource usage, CPU in use is: ${USED_CPU%.*}%, RAM in use is: ${USED_RAM%.*}%" >> $LOGPATH
+        # restart nginx 
+        sudo systemctl restart nginx > /dev/null
+        echo "$(date "+%Y-%m-%d %H:%M:%S") Retarted Nginx!" >> $LOGPATH
+        echo "$(date "+%Y-%m-%d %H:%M:%S") Sleep for 2 minutes" >> $LOGPATH
         sleep 120
     else 
-        echo "$(date "+%Y-%m-%d %H:%M:%S") RAM in use is: ${USED_RAM%.*}%" >> $LOGFILE
-        echo "$(date "+%Y-%m-%d %H:%M:%S") Sleep for 1 minute" >> $LOGFILE
+        echo "$(date "+%Y-%m-%d %H:%M:%S") CPU in use is: ${USED_CPU%.*}%, RAM in use is: ${USED_RAM%.*}%" >> $LOGPATH
+        echo "$(date "+%Y-%m-%d %H:%M:%S") Sleep for 1 minute" >> $LOGPATH
         sleep 60
     fi
 done
